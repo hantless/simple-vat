0ec1382e45226f691a88024383159901901305a3
Frédéric Arijs <farijs@wink.be>
2017-12-12 11:09:01 +0100

    Empty ServiceProvider

---
+++ src/SimplevatServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Hantless\SimpleVat;
+
+use Illuminate\Support\ServiceProvider;
+
+class SimplevatServiceProvider extends ServiceProvider
+{
+    /**
+     * Booststrap the application services.
+     *
+     */
+    public function boot() {}
+
+    /**
+     * Register the application services.
+     *
+     */
+    public function register() {}
+
+}
